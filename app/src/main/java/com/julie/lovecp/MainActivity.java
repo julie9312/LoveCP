04ba05135b51d8f3dab049b5ad6fb85923ad7794
julie9312 <67768114+julie9312@users.noreply.github.com>
2020-09-18 18:00:02 +0900

    0918

--- app/src/main/java/com/julie/lovecp/MainActivity.java
+++ app/src/main/java/com/julie/lovecp/MainActivity.java
@@ -3,6 +3,7 @@ package com.julie.lovecp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnRegister;
     Button btnStart;
     RequestQueue requestQueue;
+    String token;
 
     Intent i;
 
@@ -27,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
 
         requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        SharedPreferences sp = getSharedPreferences("regist_pref", MODE_PRIVATE);
+        String token = sp.getString("token", null);
+        if(token != null){
+            Intent i = new Intent(MainActivity.this, First.class);
+            startActivity(i);
+            finish();
+
+        }
+
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
